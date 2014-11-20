e24276f3aca3c042222977a9dbe83bc78a68006e
Asaf <geva2@illinois.edu>
2014-11-20 17:17:06 -0600

    nothing really

--- game_scene.py
+++ game_scene.py
@@ -35,13 +35,14 @@ class ProgressBar():
         self.progressbar_sprite = pygame.image.load('assets/progress_bar.png')
         self.progressindicator_sprite = pygame.image.load('assets/progress_indicator.png')
         self.progressindicator_rect= self.progressindicator_sprite.get_rect()
+        self.progressbar_rect= self.progressbar_sprite.get_rect()
         levellength = 2000
         progress = 800
         progressRatio = progress/levellength
     def render(self, screen):
         # Render normal state
 
-        screen.blit(self.progressbar_sprite,self.progressbar_sprite.get_rect())
+        screen.blit(self.progressbar_sprite,self.progressbar_rect)
         screen.blit(self.progressindicator_sprite,self.progressindicator_rect)
         
     
