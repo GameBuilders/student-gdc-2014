d5609c840c1cae24f7e1d6cd8db0a0e36d04eef9
Ryan Norby <norby2@illinois.edu>
2014-11-20 17:16:58 -0600

    Added pidgeon spritesheet

--- pidgeon.py
+++ pidgeon.py
@@ -1,13 +1,19 @@
 import pygame
+from spritesheet import *
 
 # Pidgeon class. Stores pidgeon attack and rendering information.
 class Pidgeon():
     # Constructor. Loads pidgeon assets
     def __init__(self, type):
+        img_path = None
         if (type == 0):
-            self.sprite = pygame.image.load(os.path.join('assets', 'pidgeon.png'))
-    
+            img_path = os.path.join('assets', 'pidgeon_anim.png')
+
+        self.spritesheet = Spritesheet(img_path, 64, 64)
+        
     # Render the pidgeon
     def render(self, position):
+        sprite = self.spritesheet.get_sprite(0)
+    
         screen = pygame.display.get_surface()
         screen.blit(self.sprite, position)
