24ba22c13de7a0813f162dc9e7c6b38ed3d91c16
Andrew Exo <andrewexo@gmail.com>
2014-11-20 16:11:37 -0600

    Added Pidgeon class.

---
+++ pidgeon.py
@@ -0,0 +1,13 @@
+import pygame
+
+# Pidgeon class. Stores pidgeon attack and rendering information.
+class Pidgeon():
+    # Constructor. Loads pidgeon assets
+    def __init__(self, type):
+        if (type == 0);
+            self.sprite = pygame.image.load(os.path.join('assets', 'pidgeon.png'))
+    
+    # Render the pidgeon
+    def render(self, position):
+        screen = pygame.display.get_surface()
+        screen.blit(self.sprite, position)
--- player.py
+++ player.py
@@ -1,10 +1,13 @@
 import pygame
+from pidgeon import *
 
 # Player class. Keeps track of everything we need to know about a player.
 class Player():
     # Constructor. Initialize any player variables.
     def __init__(self):
         self.position = (0, 0)
+        self.main_pidgeon = Pidgeon(0) # load starting pidgeon
+        self.pidgeons = [self.main_pidgeon]
         pass
     
     # Sets the control scheme for this player.
@@ -13,7 +16,8 @@ class Player():
     
     # Render the current state of the player.
     def render(self, screen):
-        screen = pygame.display.get_surface()
+        #screen = pygame.display.get_surface()
+        self.main_pidgeon.render(self.position)
     
     # Update the state of the player.
     # delta: Time passed (in seconds) since the previous frame.
