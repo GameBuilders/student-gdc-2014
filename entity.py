6b3d0fe8001c4b6c604e84cd24103dd3be62b8d7
Aswin Sivaraman <sivaram2@illinois.edu>
2014-11-21 01:30:51 -0600

    Bird at end of level

--- entity.py
+++ entity.py
@@ -20,19 +20,28 @@ class Entity():
     # Update the state of the entity.
     # delta: Time passed (in seconds) since the previous frame.
     def update(self, delta, player):
-        rect = pygame.Rect(self.x, self.y, self.sprite.get_width(), self.sprite.get_height())
-        if rect.colliderect(player.get_rect()):
-            # Kill the player!
-            player.die()
 
-        if self.type == 3:#Chasing spike.
-            difference = player.position[1] - 50 - self.y
-            self.y += delta * 135 * (1 if difference > 10 else (-1 if difference < -10 else 0))
-        
-        # Check for player projectiles
-        proj_to_remove = []
-        for proj in player.projectiles:
-            if rect.collidepoint(proj.x, proj.y):
-                proj_to_remove.append(proj)
-        for proj in proj_to_remove:
-            player.projectiles.remove(proj)
+        rect = pygame.Rect(self.x, self.y, self.sprite.get_width(), self.sprite.get_height())
+
+        if (self.type > -1):
+            
+            if rect.colliderect(player.get_rect()):
+                # Kill the player!
+                player.die()
+
+            if self.type == 3:#Chasing spike.
+                difference = player.position[1] - 50 - self.y
+                self.y += delta * 135 * (1 if difference > 10 else (-1 if difference < -10 else 0))
+            
+            # Check for player projectiles
+            proj_to_remove = []
+            for proj in player.projectiles:
+                if rect.collidepoint(proj.x, proj.y):
+                    proj_to_remove.append(proj)
+            for proj in proj_to_remove:
+                player.projectiles.remove(proj)
+
+        else:
+
+            if rect.colliderect(player.get_rect()):
+                
