59491e0bd02a98f137e78e8209dac449b6598bf4
Aswin Sivaraman <sivaram2@illinois.edu>
2014-11-21 01:03:28 -0600

    Parameterizing damage.

--- projectile.py
+++ projectile.py
@@ -3,13 +3,13 @@ import os
 
 class Projectile():
 
-	def __init__(self, x, y, x_vel, y_vel, sprite):
+	def __init__(self, x, y, x_vel, y_vel, damage, sprite):
 		self.x = x
 		self.y = y
 		self.x_vel = x_vel
 		self.y_vel = y_vel
 		self.sprite = sprite
-		self.damage = 25
+		self.damage = damage
 
 	def update(self,delta):
 		self.x += delta * self.x_vel
